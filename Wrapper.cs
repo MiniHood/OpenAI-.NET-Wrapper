6b96a9413d834279e90c41a61f4bcbd818d2e348
MiniHood <75768388+MiniHood@users.noreply.github.com>
2023-02-09 23:08:59 +0000

    Added Temperature, Engine and Token parameters

--- Wrapper.cs
+++ Wrapper.cs
@@ -4,59 +4,66 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-class OpenAI
+namespace OpenAIWrapper
 {
-    private readonly string _apiKey;
-    private readonly HttpClient _httpClient = new HttpClient();
-    private readonly string _baseUrl = "https://api.openai.com/v1/engines/gpt-3/jobs";
-
-    public OpenAI(string apiKey)
+    public class OpenAI
     {
-        _apiKey = apiKey;
-    }
+        private readonly string _apiKey;
+        private readonly string _engine;
+        private readonly HttpClient _httpClient;
 
-    public async Task<string> GenerateText(string prompt, int maxTokens = 100)
-    {
-        var request = new GenerateTextRequest
+        public OpenAI(string apiKey, string engine)
         {
-            Prompt = prompt,
-            MaxTokens = maxTokens
-        };
-
-        var response = await PostJsonAsync("generate", request);
-        var generateTextResponse = JsonConvert.DeserializeObject<GenerateTextResponse>(response);
-
-        return generateTextResponse.Choices[0].Text;
-    }
-
-    private async Task<string> PostJsonAsync(string endpoint, object content)
-    {
-        _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_apiKey}");
-        _httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
-
-        var response = await _httpClient.PostAsync($"{_baseUrl}/{endpoint}", new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json"));
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new Exception($"Request to OpenAI failed with status code {response.StatusCode}: {await response.Content.ReadAsStringAsync()}");
+            _apiKey = apiKey;
+            _engine = engine;
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.openai.com/v1/")
+            };
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_apiKey}");
         }
 
-        return await response.Content.ReadAsStringAsync();
+        public async Task<string> GenerateText(string prompt, int maxTokens = 100, float temperature = 1.0f)
+        {
+            var request = new GenerateTextRequest
+            {
+                Engine = _engine,
+                Prompt = prompt,
+                MaxTokens = maxTokens,
+                Temperature = temperature
+            };
+
+            var response = await PostJsonAsync("engines/engine/generate", request);
+            var generateTextResponse = JsonConvert.DeserializeObject<GenerateTextResponse>(response);
+
+            return generateTextResponse.Choices[0].Text;
+        }
+
+        private async Task<string> PostJsonAsync(string url, object data)
+        {
+            var json = JsonConvert.SerializeObject(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
     }
-}
 
-class GenerateTextRequest
-{
-    public string Prompt { get; set; }
-    public int MaxTokens { get; set; }
-}
+    class GenerateTextRequest
+    {
+        public string Engine { get; set; }
+        public string Prompt { get; set; }
+        public int MaxTokens { get; set; }
+        public float Temperature { get; set; }
+    }
 
-class GenerateTextResponse
-{
-    public Choice[] Choices { get; set; }
-}
+    class GenerateTextResponse
+    {
+        public Choice[] Choices { get; set; }
+    }
 
-class Choice
-{
-    public string Text { get; set; }
+    class Choice
+    {
+        public string Text { get; set; }
+    }
 }
